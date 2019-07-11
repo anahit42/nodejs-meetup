f8b882f9e0498e1108ea4d20e0d8bf85d52b0f39
Anahit Vardanyan <vardanian.anahit@gmail.com>
2019-07-11 13:19:20 +0400

    Add logging in service-mailer consumer

--- service-mailer/consumer.js
+++ service-mailer/consumer.js
@@ -6,7 +6,16 @@ const MailerLib = require('./libs/mailer');
 
 kafkaClient(async (message) => {
   try {
-    await MailerLib.sendUserRegistrationEmail(message.email, message.data);
+    const { email_type } = message;
+    console.log('Consumed message type', email_type);
+
+    switch (email_type) {
+      case 'account-create':
+        await MailerLib.sendUserRegistrationEmail(message.email, message.data);
+        break;
+      default:
+        console.log(`${email_type} message type is not supported.`)
+    }
   } catch (err) {
     console.log('Error handler', err);
   }
